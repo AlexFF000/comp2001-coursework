0dd5dd55cb641f50c35a880b53205d81a7d619b5
Alex Redmond <42391315+AlexFF000@users.noreply.github.com>
2020-12-20 18:34:54 +0000

    Recreated project using .NET Core 3.1 rather than 2.1 due to package compatibility errors

--- AuthAPI/Startup.cs
+++ AuthAPI/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
@@ -10,6 +6,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace AuthAPI
 {
