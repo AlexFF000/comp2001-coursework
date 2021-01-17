039cd16552164fc351cb2bcdb27ab0abbc5a7b97
Alex Redmond <42391315+AlexFF000@users.noreply.github.com>
2021-01-17 11:46:43 +0000

    Moved connection string and private key out of code and into appsettings.json

--- AuthAPI/Startup.cs
+++ AuthAPI/Startup.cs
@@ -27,19 +27,7 @@ namespace AuthAPI
 {
     public class Startup
     {
-        public static string PrivateKey = @"MIICXgIBAAKBgQCcKH795t691X/OE53Vr+jl4jtrR8ynrbX8FXKoOoprMuGQ20uH
-W8/BRnrNSNHyyz3iYWXAh54ePkIZe6DZTyYW++PFNX+qFv9nOCtCwc3QbZvXNvFr
-y1H06xx0sfM5EX84SYEeIs3fTasrbd27kljUULWI5szh7WGCJbASF6MxYQIDAQAB
-AoGBAI3aJ4njiCEv6To9DZqIgmsZOFq6zUjgfrkwjogNLaI1hTv+STz7hN0Qdgs3
-BCimwV98lF7xkwvsCEV3zWS6BmFZv/aL85FIF0Pv2TYA0FlYd3pQtO0hyPgww+Ff
-Vn8GXZxdrjSS32W0/BJPJ69N+hOD2GVSxJ7VvBsTqnwWg9w1AkEA5erX+6LJ7w3S
-ZHv6XYSN4dhjfzvu901aZfr+3ezg5zBdxA6C8ZCzrgfMSjXXEl0+5k9OWoH18AXe
-efOLN7i5NwJBAK3fkmCekwEkJYNZUt3kezSQ6vJaaDkin3mVpAhBOpkeqtyTF7g6
-m/0h92SqUfc06TRAnVwyJWqdt05mSect1icCQQCwpEhCBWTdrCHbn8DP/po/TM/M
-nhBPofd06GzNtkNoe6leisOhsknJyCX1Uf4mFsvPiFirgrOgL/IogauRKPPrAkEA
-j6sUwFFHFsJPXOEJ399TDnRQLkEVrWA2dbh3+XoseSkl9Wx1RPzot5jBWqSBZcqK
-h2p8nBmzYQ1A0b8jgaoWMQJAPHxNEyky16ubU38cEDtnOGJHD7gNNIhwnsfnvKaQ
-oa2YYSv2/gsdSiDBbiSy0wWgPMPLHweOBdmDLvVLQsL9xw==";
+        public static string PrivateKey;
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -51,8 +39,10 @@ oa2YYSv2/gsdSiDBbiSy0wWgPMPLHweOBdmDLvVLQsL9xw==";
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1).AddXmlDataContractSerializerFormatters();
-            services.AddDbContext<COMP2001_ARedmondContext>();
+            services.AddDbContext<COMP2001_ARedmondContext>(options => options.UseSqlServer(Configuration.GetConnectionString("socem1")));
             services.AddControllers();
+            // Fetch private key used for signing tokens from appsettings
+            PrivateKey = Configuration.GetValue<string>("PrivateKey");
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => 
             {
                 options.TokenValidationParameters = new TokenValidationParameters
