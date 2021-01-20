3470dfc0c455ee9151b558384456709d829c18d8
Alex Redmond <42391315+AlexFF000@users.noreply.github.com>
2021-01-20 23:56:37 +0000

    Added error handling to processDataset.php

--- WebApplication/processDataset.php
+++ WebApplication/processDataset.php
@@ -4,13 +4,24 @@ const SCHEMA = "https://schema.org";
 
 function getJSONLD($dataFilePath){
     // Function to be called from other PHP files to return the dataset as a JSON-LD PHP array
-    return toJSONLD(readData($dataFilePath));
+    // Override default error handler to allow warnings to be handled
+    set_error_handler("handleExceptions");
+    try {
+        $data = toJSONLD(readData($dataFilePath));
+        restore_error_handler();
+        return $data;
+    }
+    catch (ErrorException $e){
+        // If the data cannot be parsed, simply return an empty array
+        restore_error_handler();
+        return [];
+    }
 }
 
 function readData($dataFilePath){
     // Read the data in from the file and parse into a PHP object
     $file = fopen($dataFilePath, "r");
-    $rawData = fread($file, filesize(FILEPATH));
+    $rawData = fread($file, filesize($dataFilePath));
     return json_decode($rawData, true);
 }
 
@@ -32,4 +43,9 @@ function toRoute($data){
         "line"=> $data["geometry"]["coordinates"]
         ]
     ];
+}
+
+function handleExceptions($severity, $message, $file, $line, array $context = []){  // Do not accept Throwable to maintain compatibility with PHP > 7, provide a default for $context because it is deprecated in PHP 7.2 so will not be passed
+    // Upgrade warnings to exceptions so they can be properly caught and handled
+    throw new ErrorException($message, 0, $severity, $file);
 }
