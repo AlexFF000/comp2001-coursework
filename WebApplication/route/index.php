83d5284a9315f6eca4e37da9615e5bf93096979a
Alex Redmond <42391315+AlexFF000@users.noreply.github.com>
2021-01-23 11:42:58 +0000

    Moved dataset into assets folder

--- WebApplication/route/index.php
+++ WebApplication/route/index.php
@@ -1,7 +1,7 @@
 <?php
 // Return the JSON-LD data to the user
 require_once("../processDataset.php");
-const DATAFILE = "../plymouth rights of way dataset.geojson";
+const DATAFILE = "../assets/dataset/plymouth rights of way dataset.geojson";
 
 $data = json_encode(getJSONLD(DATAFILE));
 header("Content-Type: application/json; charset=utf8");
